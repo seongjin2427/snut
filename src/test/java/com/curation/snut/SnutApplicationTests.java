56e6e378a0474536c255e5cc5801cab36bf035af
seongjin <seongjin2427@gmail.com>
2022-03-04 23:45:31 +0900

    convert to spring+vue project

---
+++ src/test/java/com/curation/snut/SnutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curation.snut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
