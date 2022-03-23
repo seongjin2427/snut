606a00f2824b4cd1c232c0a7c090db49b15fb899
seongjin <seongjin2427@gmail.com>
2022-03-23 23:55:56 +0900

    making MainWithLogin page (ing, 1st commit)

---
+++ vue/src/components/CommonButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="button" :style="{ 
+    width: width+'px', 
+    height: height+'px', 
+    marginTop: marginTop+'px' , 
+    marginRight: marginRight+'px' }">
+    {{ buttonName }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommonButton",
+  props: ['buttonName', 'width', 'height', 'margin-top', 'margin-right'],
+}
+</script>
+
+<style scoped>
+.button {  
+  font-family: AlegreyaSans, sans-serif;
+  min-width: 80px;
+  min-height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px black solid;
+  border-radius: 20px;
+  font-weight: bold;
+  font-size: 14px;
+  /* letter-spacing: -1px; */
+  cursor: pointer;
+}
+</style>
--- vue/src/views/MainWithLogin.vue
+++ vue/src/views/MainWithLogin.vue
@@ -1,51 +1,69 @@
 <template>
-  <div>
-    <img class="main-logo-text" src="@/assets/logo_text.png" alt="logo_text">
-    <input class="main-search-input" type="text" placeholder="SEARCH">
-    <h1>MainWithLogin 페이지 입니다</h1>
-    <main-button buttonName="Log in" />
+  <div class="main-with-login">
+    <div class="main-with-login-body">
+      <header>
+        <img class="main-logo-text" src="@/assets/logo_text.png" alt="text_logo">
+        <input-box class="main-input" placeholder="SEARCH" width="30" height="38" />
+        <div class="button-area">
+          <common-button buttonName="Log in" width="80" height="35" marginTop="5" marginRight="20" />
+          <common-button buttonName="Register" width="80" height="35" marginTop="5" marginRight="42" />
+          <img src="@/assets/btn_hamburger.png" alt="nav_btn">
+        </div>
+      </header>
+    </div>
   </div>
 </template>
 
 <script>
-import MainButton from "@/components/MainButton.vue"
+import CommonButton from '../components/CommonButton.vue'
+import InputBox from '../components/InputBox.vue'
+
 
 export default {
-  name: "MainWithLogin",
-  components: {
-    MainButton
-  }
+  components: { InputBox, CommonButton },
+  name: "MainWithLogin"
 }
 </script>
 
 <style scoped>
+.main-with-login-body {
+  max-width: 1200px;
+  min-width: 1200px;
+  width: 100vw;
+  height: 100vh;
+  /* background: lightcoral; */
+  margin: 0 auto;
+}
+header {
+  width: 100%;
+  height: 200px;
+  /* background: lightblue; */
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+}
 .main-logo-text {
-    height: 59px;
-    margin-left: 117px;
-    margin-top: 49px;
-    width: 169px;
+  width: 113px;
+  height: 39px;
+  margin-top: 60px;
+  position: absolute;
+  left: 0;
+  top: 0;
 }
-input:focus {
-  outline: none;
+.main-input {
+  margin-top: 67px;
+  margin-right: 5%;
 }
-.main-search-input {
-    top: 72px;
-    left: 702px;
-    width: 510px;
-    height: 40px;
-    padding: 0 15px;
-    border-color: #00000099;
-    border-style: solid;
-    border-width: 0.5px;
-    letter-spacing: 0.00px;
-    line-height: normal;
-    mix-blend-mode: normal;
-    position: absolute;
-    resize: none;
-    text-align: left;
-    background-color: rgb(232, 236, 230);
+.button-area {
+  width: 30%;
+  /* background: red; */
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  margin-top: 63px;
 }
-.main-search-input::placeholder {
-  color: #00000099;
+.button-area img {
+  margin-right: 20px;
+  cursor: pointer;
 }
 </style>
