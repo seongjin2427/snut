192cd32299a0c46d366732568e9092f0251c407a
soyeni6575 <soyeni6575@gmail.com>
2022-03-26 23:57:18 +0900

    0323

--- vue/src/views/Faq.vue
+++ vue/src/views/Faq.vue
@@ -1,12 +1,26 @@
 <template>
-<div class="faq">
-  <header>
-    <div class="faqp">
-      <img class="homebu" src="@/assets/logo_text.png" alt="logo_text">
-      <div class="text-faq">FAQ</div>
-    </div>
-  </header>
-</div>
+  <div class="faq-page">
+    <!--  header-->
+    <header>
+      <div class="faqp">
+        <img class="homebu" src="@/assets/logo_text.png" alt="logo_text">
+        <div class="text-faq">FAQ</div>
+      </div>
+    </header>
+    <!--  expandable-faqlist-->
+
+
+      <div class="expandable">
+        <div class="expandable-title-wrapper">
+          <p class="expandable-title">01 title</p>
+          <div class="expandable-text-wrapper">
+            <p class="expandable-text">texting</p>
+          </div>
+        </div>
+      </div>
+
+
+  </div>
 </template>
 
 <script>
@@ -16,10 +30,11 @@ export default {
 </script>
 
 <style scoped>
-.faq{
+.faq {
   width: 100%;
 }
-header{
+
+header {
   height: 160px;
   width: 100%;
   background: #EECAC6;
@@ -28,7 +43,8 @@ header{
   justify-content: center;
 
 }
-.faqp{
+
+.faqp {
   width: 90%;
   max-width: 1200px;
   margin: 0 auto;
@@ -39,13 +55,15 @@ header{
   justify-content: center;
 
 }
-.homebu{
+
+.homebu {
   width: 113px;
   height: 39px;
   position: absolute;
   left: 5px;
 }
-.text-faq{
+
+.text-faq {
   display: flex;
   position: relative;
   text-align: center;
@@ -57,4 +75,30 @@ header{
   color: #F6F6F6;
 
 }
+
+.expandable {
+  cursor: s-resize;
+  padding-top: 300px;
+  font-size: 30px;
+  bottom-border: 3px solid #292f6d;
+  text-align: center;
+
+}
+
+.expandable-title-wrapper {
+  display: flex;
+  justify-content: space-between;
+
+}
+
+.expandable-title {
+  display: flex;
+  align-items: center;
+  font-family: 'Noto-sans KR', 'Apple SD Gothic Neo', 'Alegreya Sans', sans-serif;
+  font-weight: 600;
+  font-size: 30px;
+  color: #292f6d;
+}
+
+
 </style>
