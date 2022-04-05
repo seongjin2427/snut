10d897d0a26f0fb0c589551a533ebb92a9d0638f
Geon A <guna0901@gmail.com>
2022-04-05 17:50:21 +0900

    api server test

--- src/main/java/com/curation/snut/controller/CommunityController.java
+++ src/main/java/com/curation/snut/controller/CommunityController.java
@@ -6,6 +6,7 @@ import com.curation.snut.dto.CommunityDTO;
 import com.curation.snut.service.CommentService;
 import com.curation.snut.service.CommunityService;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/curation/snut/controller/ControllerTest.java
+++ src/main/java/com/curation/snut/controller/ControllerTest.java
@@ -3,28 +3,50 @@ package com.curation.snut.controller;
 import java.util.List;
 
 import com.curation.snut.dto.CommunityDTO;
-import com.curation.snut.repository.CommunityRepository;
 import com.curation.snut.service.CommentService;
 import com.curation.snut.service.CommunityService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
 @RestController
+@RequiredArgsConstructor
 public class ControllerTest {
-    @Autowired
-    private CommentService commentService;
-    @Autowired
-    private CommunityService communityService;
+    private final CommentService commentService;
+    private final CommunityService communityService;
 
-    @RequestMapping(value = "/test/commuList", method = RequestMethod.GET)
-    public List<CommunityDTO> test() throws Exception {
-        return communityService.communityListWithCnt();
+    // @PreAuthorize("hasRole('ADMIN')")
+    // @PreAuthorize("#Member != null && #Member.email eq \"123@123.123\"")
+
+    @PreAuthorize("permitAll()")
+    @GetMapping(value = "/test/commuList", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CommunityDTO>> testList(String searchTitle) {
+        if (searchTitle != null) {
+            List<CommunityDTO> searchCommunityList = communityService.searchTitle(searchTitle);
+            return new ResponseEntity<>(searchCommunityList, HttpStatus.OK);
+        } else {
+            List<CommunityDTO> list = communityService.communityListWithCnt();
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(value = "/test/commuList", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> testWrite(CommunityDTO communityDTO) {
+        communityService.write(communityDTO);
+        return new ResponseEntity<>("완료", HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/test/commuList/delete")
+    public ResponseEntity<String> testdelete(Long no) {
+        communityService.delete(no);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
